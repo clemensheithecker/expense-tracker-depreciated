66872b782969babc724b617c03d950de3e4fb50c
Clemens Stefan Heithecker <clemens@heithecker.dev>
2023-01-08 12:53:41 +0100

    Add extra page layout padding on big screen sizes

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -11,7 +11,7 @@ export const DefaultLayout = ({ children }: DefaultLayoutProps) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="mx-auto max-w-screen-lg p-4">{children}</main>
+      <main className="mx-auto max-w-screen-lg p-4 sm:p-8">{children}</main>
     </>
   );
 };
