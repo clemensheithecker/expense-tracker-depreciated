f2cb0890b4738b450dace3f238b76b0eabaaef01
Clemens Stefan Heithecker <clemens@heithecker.dev>
2023-01-12 22:43:04 +0100

    Add "Add Transaction" button

--- src/pages/account/[id].tsx
+++ src/pages/account/[id].tsx
@@ -96,7 +96,17 @@ const AccountItem = (props: {
           </dd>
         </dl>
       </div>
-      <h2 className="mt-8 text-xl font-bold text-gray-900">Transactions</h2>
+      <div className="mt-8 flex items-center justify-between">
+        <h2 className="text-xl font-bold text-gray-900">Transactions</h2>
+        <div>
+          <button
+            type="button"
+            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Add transaction
+          </button>
+        </div>
+      </div>
       <TransactionPreviewItems transactions={transactions} />
       <div className="mt-8">
         <MetaDataFooter listItems={metaDataListItems} />
