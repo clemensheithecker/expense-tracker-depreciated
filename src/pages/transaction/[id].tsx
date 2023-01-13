4e3413ec8443bed0c37cd921ecb81452c6b8e8e7
Clemens Stefan Heithecker <clemens@heithecker.dev>
2023-01-13 01:01:38 +0100

    Update styling of transaction `Edit` button

--- src/pages/transaction/[id].tsx
+++ src/pages/transaction/[id].tsx
@@ -125,7 +125,7 @@ const TransactionItem = (props: { transaction: TransactionByIdOutput }) => {
         <div className="flex justify-end border-t px-4 py-5 sm:px-6">
           <button
             type="button"
-            className="inline-flex justify-center rounded-md border border-transparent bg-gray-100 py-2 px-4 text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            className="rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
             Edit
           </button>
