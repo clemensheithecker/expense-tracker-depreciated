2199145b0ecc73e84870dddfec63aeca10fd1e8d
Clemens Stefan Heithecker <clemens@heithecker.dev>
2023-01-13 01:17:31 +0100

    Update markup of account pages

--- src/pages/account/[id].tsx
+++ src/pages/account/[id].tsx
@@ -103,7 +103,7 @@ const AccountItem = (props: {
   return (
     <>
       <PageHeader header={account.name} subheader="Account" />
-      <div className="mt-8 grid grid-cols-2 gap-4 sm:grid-cols-3">
+      <section className="mt-8 grid grid-cols-2 gap-4 sm:grid-cols-3">
         {statsItems.map((statsItem) => (
           <StatsItem
             key={statsItem.key}
@@ -112,22 +112,24 @@ const AccountItem = (props: {
             color={statsItem.color}
           />
         ))}
-      </div>
-      <div className="mt-8 flex items-center justify-between">
-        <h2 className="text-xl font-bold text-gray-900">Transactions</h2>
-        <div>
-          <button
-            type="button"
-            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            Add transaction
-          </button>
+      </section>
+      <section className="mt-8">
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl font-bold text-gray-900">Transactions</h2>
+          <div>
+            <button
+              type="button"
+              className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Add transaction
+            </button>
+          </div>
         </div>
-      </div>
-      <TransactionPreviewItems transactions={transactions} />
-      <div className="mt-8">
+        <TransactionPreviewItems transactions={transactions} />
+      </section>
+      <section className="mt-8">
         <MetaDataFooter listItems={metaDataListItems} />
-      </div>
+      </section>
     </>
   );
 };
