07d8ebd1a79aefe2c760562881d87cb0b031dc88
Clemens Stefan Heithecker <clemens@heithecker.dev>
2023-01-24 23:20:42 +0100

    Update `Modal` and add `AddTransactionModal`

--- src/components/AddTransactionModal.tsx
+++ src/components/AddTransactionModal.tsx
@@ -1,5 +1,9 @@
 import { Dialog } from '@headlessui/react';
 import Modal from '~/components/Modal';
+import SelectInput from './Form/SelectInput';
+import InputWithAddOn from './Form/InputWithAddOn';
+import Input from './Form/Input';
+import Textarea from './Form/Textarea';
 
 const AddTransactionModal = (props: {
   isOpen: boolean;
@@ -9,22 +13,94 @@ const AddTransactionModal = (props: {
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-        <div className="sm:flex sm:items-start">
-          <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-            <Dialog.Title
-              as="h3"
-              className="text-lg font-medium leading-6 text-gray-900"
-            >
-              Deactivate account
-            </Dialog.Title>
-            <div className="mt-2">
-              <p className="text-sm text-gray-500">
-                Are you sure you want to deactivate your account? All of your
-                data will be permanently removed. This action cannot be undone.
-              </p>
+      <div className="flex flex-col items-start">
+        <Dialog.Title as="h3" className="text-lg font-medium leading-6">
+          Add New Transaction
+        </Dialog.Title>
+        <div className="mt-2">
+          <p className="text-sm text-gray-500 dark:text-gray-400">
+            Add a new transaction to your account.
+            {/* Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Ipsa libero labore natus, voluptatum,
+            quod voluptates, quos quia voluptatibus quibusdam tempora quas.
+            Quisquam, quod. Quod, quia */}
+          </p>
+        </div>
+        <div className="mt-5 md:col-span-2">
+          <form action="#" method="POST">
+            <div className="grid grid-cols-6 gap-6">
+              <div className="col-span-6 sm:col-span-3">
+                <SelectInput
+                  label="Account"
+                  name="account"
+                  options={['Household', 'Car', 'Travel']}
+                />
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <InputWithAddOn
+                  label="Amount"
+                  name="amount"
+                  placeholder="0.00"
+                />
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label
+                  htmlFor="date"
+                  className="block text-sm font-medium text-gray-700 dark:text-gray-100"
+                >
+                  Date
+                </label>
+                <input
+                  type="text"
+                  name="date"
+                  id="date"
+                  autoComplete="date"
+                  className="mt-1 block w-full rounded-md border-gray-300 bg-white shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 sm:text-sm"
+                />
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label
+                  htmlFor="time"
+                  className="block text-sm font-medium text-gray-700 dark:text-gray-100"
+                >
+                  Time
+                </label>
+                <input
+                  type="text"
+                  name="time"
+                  id="time"
+                  autoComplete="time"
+                  className="mt-1 block w-full rounded-md border-gray-300 bg-white shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 sm:text-sm"
+                />
+              </div>
+
+              <div className="col-span-6 sm:col-span-4">
+                <Input label="Involved party" name="involved-party" />
+              </div>
+
+              <div className="col-span-6">
+                <Textarea
+                  label="Note"
+                  name="note"
+                  placeholder="You can add a note here."
+                />
+                <p className="mt-2 text-sm text-gray-500">
+                  Brief description for your transaction.
+                </p>
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <SelectInput
+                  label="Payment method"
+                  name="payment-method"
+                  options={['Cash', 'PayPal']}
+                />
+              </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </Modal>
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -31,7 +31,7 @@ const Modal = (props: {
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -41,38 +41,20 @@ const Modal = (props: {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-lg font-medium leading-6 text-gray-900"
-                      >
-                        Deactivate account
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          Are you sure you want to deactivate your account? All
-                          of your data will be permanently removed. This action
-                          cannot be undone.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all dark:bg-gray-900 sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pt-5 pb-4 dark:bg-gray-900 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">{children}</div>
                 </div>
-                <div>{children}</div>
-                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <div className="bg-gray-50 px-4 py-3 dark:bg-gray-800 sm:flex sm:flex-row-reverse sm:px-6">
                   <button
-                    type="button"
-                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={onClose}
+                    type="submit"
+                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
                   >
-                    Deactivate
+                    Save
                   </button>
                   <button
                     type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 dark:hover:bg-gray-800 dark:focus:ring-offset-gray-900 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                     onClick={onClose}
                     ref={cancelButtonRef}
                   >
